55279a150017f93782d31ee4b5cd743f487f7643
fasseg <frank.asseg@googlemail.com>
2012-07-19 17:04:00 +0200

    added image creation method

--- src/test/java/eu/scapeproject/ConnectorAPIMockTest.java
+++ src/test/java/eu/scapeproject/ConnectorAPIMockTest.java
@@ -4,11 +4,14 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import java.io.ByteArrayOutputStream;
+import java.net.URI;
+import java.util.Arrays;
 import java.util.Date;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ByteArrayEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -16,15 +19,18 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import eu.scapeproject.model.Agent;
 import eu.scapeproject.model.IntellectualEntity;
 import eu.scapeproject.model.UUIDIdentifier;
 import eu.scapeproject.model.metadata.dc.DCMetadata;
+import eu.scapeproject.model.metadata.textmd.TextMDMetadata.Encoding.EncodingAgent.Role;
 import eu.scapeproject.model.mets.MetsFactory;
 
 public class ConnectorAPIMockTest {
 
 	private static final ConnectorAPIMock MOCK = new ConnectorAPIMock();
 	private static final String MOCK_URL = "http://localhost:" + MOCK.getPort() + "/";
+	private static final HttpClient CLIENT = new DefaultHttpClient();
 
 	@BeforeClass
 	public static void setup() throws Exception {
@@ -43,13 +49,15 @@ public class ConnectorAPIMockTest {
 	}
 
 	@Test
-	public void testInvalidIE() throws Exception {
-		HttpResponse resp = ConnectorAPIUtil.getInstance().getEntity("non-existant");
+	public void testInvalidIntellectualEntity() throws Exception {
+		HttpGet get = ConnectorAPIUtil.getInstance().createGetEntity("non-existant");
+		HttpResponse resp = CLIENT.execute(get);
 		assertTrue(resp.getStatusLine().getStatusCode() == 404);
+		get.releaseConnection();
 	}
-		
+
 	@Test
-	public void testIngestEmptyIntelletualEntity() throws Exception {
+	public void testIngestEmptyIntellectualEntity() throws Exception {
 		IntellectualEntity ie = new IntellectualEntity.Builder()
 				.identifier(new UUIDIdentifier())
 				.descriptive(new DCMetadata.Builder()
@@ -58,11 +66,31 @@ public class ConnectorAPIMockTest {
 						.language("en")
 						.build())
 				.build();
-		HttpResponse resp = ConnectorAPIUtil.getInstance().postEntity(ie);
+		HttpPost post = ConnectorAPIUtil.getInstance().createPostEntity(ie);
+		HttpResponse resp = CLIENT.execute(post);
 		assertTrue(resp.getStatusLine().getStatusCode() == 201);
-		resp = ConnectorAPIUtil.getInstance().getEntity(ie.getIdentifier().getValue());
-		IOUtils.copy(resp.getEntity().getContent(),System.out);
+		post.releaseConnection();
+
+		HttpGet get = ConnectorAPIUtil.getInstance().createGetEntity(ie.getIdentifier().getValue());
+		resp = CLIENT.execute(get);
 		assertTrue(resp.getStatusLine().getStatusCode() == 200);
+		get.releaseConnection();
+	}
+
+	@Test
+	public void testIngestImage() throws Exception {
+		IntellectualEntity entity = ModelUtil.createEntity(Arrays.asList(ModelUtil.createImageRepresentation(URI
+				.create("https://a248.e.akamai.net/assets.github.com/images/modules/about_page/octocat.png?1315937507"))));
+		HttpPost post = ConnectorAPIUtil.getInstance().createPostEntity(entity);
+		HttpResponse resp = CLIENT.execute(post);
+		assertTrue(resp.getStatusLine().getStatusCode() == 201);
+		post.releaseConnection();
+
+		HttpGet get = ConnectorAPIUtil.getInstance().createGetEntity(entity.getIdentifier().getValue());
+		resp = CLIENT.execute(get);
+		IOUtils.copy(resp.getEntity().getContent(), System.out);
+		assertTrue(resp.getStatusLine().getStatusCode() == 200);
+		get.releaseConnection();
 	}
 
 }
--- src/test/java/eu/scapeproject/ConnectorAPIUtil.java
+++ src/test/java/eu/scapeproject/ConnectorAPIUtil.java
@@ -1,15 +1,10 @@
 package eu.scapeproject;
 
-import static org.junit.Assert.assertTrue;
-
 import java.io.ByteArrayOutputStream;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ByteArrayEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
 
 import eu.scapeproject.model.IntellectualEntity;
 import eu.scapeproject.model.mets.MetsFactory;
@@ -19,8 +14,6 @@ public class ConnectorAPIUtil {
 	private static final String ENTITY_PATH="/entity";
 	private static ConnectorAPIUtil INSTANCE;
 
-	private final HttpClient CLIENT=new DefaultHttpClient();
-	
 	private ConnectorAPIUtil(){
 		super();
 	}
@@ -32,22 +25,17 @@ public class ConnectorAPIUtil {
 		return INSTANCE;
 	}
 
-	public HttpResponse postEntity(IntellectualEntity ie) throws Exception{
+	public HttpPost createPostEntity(IntellectualEntity ie) throws Exception{
         HttpPost post=new HttpPost(MOCK_URL + ENTITY_PATH);
         MetsFactory factory=MetsFactory.getInstance();
         ByteArrayOutputStream bos=new ByteArrayOutputStream();
         factory.serialize(ie, bos);
         post.setEntity(new ByteArrayEntity(bos.toByteArray()));
-        HttpResponse resp=CLIENT.execute(post);
-        post.releaseConnection();
-        return resp;
+        return post;
     }
 
-	public HttpResponse getEntity(String id) throws Exception{
-		HttpGet get=new HttpGet(MOCK_URL + ENTITY_PATH + "/" + id);
-        HttpResponse resp=CLIENT.execute(get);
-        get.releaseConnection();
-        return resp;
+	public HttpGet createGetEntity(String id) throws Exception{
+		return new HttpGet(MOCK_URL + ENTITY_PATH + "/" + id);
 	}
 
 }
