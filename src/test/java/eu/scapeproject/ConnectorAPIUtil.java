d80afe38541c2b63316d8b0916d1a9297d278fdc
fasseg <frank.asseg@googlemail.com>
2012-07-19 16:17:40 +0200

    added renamed files

---
+++ src/test/java/eu/scapeproject/ConnectorAPIUtil.java
@@ -0,0 +1,53 @@
+package eu.scapeproject;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import eu.scapeproject.model.IntellectualEntity;
+import eu.scapeproject.model.mets.MetsFactory;
+
+public class ConnectorAPIUtil {
+	private static final String MOCK_URL="http://localhost:8783";
+	private static final String ENTITY_PATH="/entity";
+	private static ConnectorAPIUtil INSTANCE;
+
+	private final HttpClient CLIENT=new DefaultHttpClient();
+	
+	private ConnectorAPIUtil(){
+		super();
+	}
+	
+	public static ConnectorAPIUtil getInstance() {
+		if (INSTANCE == null){
+			INSTANCE = new ConnectorAPIUtil();
+		}
+		return INSTANCE;
+	}
+
+	public HttpResponse postEntity(IntellectualEntity ie) throws Exception{
+        HttpPost post=new HttpPost(MOCK_URL + ENTITY_PATH);
+        MetsFactory factory=MetsFactory.getInstance();
+        ByteArrayOutputStream bos=new ByteArrayOutputStream();
+        factory.serialize(ie, bos);
+        post.setEntity(new ByteArrayEntity(bos.toByteArray()));
+        HttpResponse resp=CLIENT.execute(post);
+        post.releaseConnection();
+        return resp;
+    }
+
+	public HttpResponse getEntity(String id) throws Exception{
+		HttpGet get=new HttpGet(MOCK_URL + ENTITY_PATH + "/" + id);
+        HttpResponse resp=CLIENT.execute(get);
+        get.releaseConnection();
+        return resp;
+	}
+
+}
